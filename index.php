90fca540b40b8054c9b35bc846d84b1469137d09
wenxuancheung <307335319@qq.com>
2018-05-30 11:49:20 +0800

    气球飘动，星星闪动

---
+++ index.php
@@ -0,0 +1,180 @@
+<div class="top">
+	<img src="<?php echo NIANHUI_STATIC?>img/ball.png" class="top-img"/>
+	<img src="<?php echo NIANHUI_STATIC?>img/smallBall.png" class="bottom-img"/>
+	<img src="<?php echo NIANHUI_STATIC?>img/star.png" class="className" style="position: relative;left: -72px;top: 150px;"/>
+	<img src="<?php echo NIANHUI_STATIC?>img/starTwo.png" class="className" style="position: relative;left: 505px;top: 160px;" />
+	<img src="<?php echo NIANHUI_STATIC?>img/starTwo.png" class="className" style="position: relative;left: -150px;top: 20px;" />
+	<img src="<?php echo NIANHUI_STATIC?>img/starThird.png" class="className" style="position: relative;left: -355px;top: 300px;" />
+	<img src="<?php echo NIANHUI_STATIC?>img/starThird.png" class="className" style="position: relative;left: 250px;top: 350px;" />
+	<div class="top-text">
+		<!-- <div class="top-text-lead">导语:</div> -->
+		<div class="top-text-content">
+			<div class="top-text-content-title">
+				<!-- “<font class="top-text-content-font">全域旅游体验达人</font>”<font>计划</font> -->
+				<font style="background-color: #ffc640;border-radius: 10px;">“<font class="top-text-content-font" >全域旅游体验达人</font>”<font>计划&nbsp;</font></font>
+			</div>
+			<div class="top-text-content-main">
+				为了营造惠州规范有序的旅游市场环境，完善旅游的服务质量，提升旅游体验。在这里，我们细心聆听你提出的建议与反馈惠州旅游服务质量，从“吃、住、行、游、购、娱”进一步提升惠州旅游质量水平。提出你的惠州旅游改善反馈吧，从加入“全域旅游体验达人”开始！
+			</div>
+		</div>
+	</div>
+</div>
+<div class="center">
+	<video src="<?php echo NIANHUI_STATIC?>video/xizi.mp4" controls="controls" width="750" height="420" poster="<?php echo NIANHUI_STATIC?>img/video-bg.jpg">
+	</video>
+	<div class="center-option">
+		<div ><font style="background-color: #ffc640;border-radius: 10px;font-size: 30px;">&nbsp;惠州旅行家，我想听你说&nbsp;</font></div>
+		<ul class="center-option-content" v-if="type === 1">
+			<a href="http://bbs.xizi.com/forum-27-1.html" target="_blank">
+				<li class="center-option-main"></li>
+			</a>
+			<a href="http://bbs.xizi.com/thread-4383403-1-1.html" target="_blank">
+				<li class="center-option-mainContent middle ">
+					<img src="<?php echo NIANHUI_STATIC?>img/hz.png" class="center-option-mainContent-img" />
+					<div class="center-option-bg-one" ></div>
+					<div class="center-option-mainContent-font">
+						惠州旅游，你的进步空间还很大！
+					</div>
+				</li>
+			</a>
+			<a href="http://bbs.xizi.com/thread-4379627-1-1.html" target="_blank">
+				<li class="center-option-mainContent">
+					<img src="<?php echo NIANHUI_STATIC?>img/hc.png" class="center-option-mainContent-img"/>
+					<div class="center-option-bg-two"></div>
+					<div class="center-option-mainContent-font">
+						惠州，距离国际化旅游城市并不远
+					</div>
+				</li>
+			</a>
+			<a href="http://bbs.xizi.com/thread-4385280-1-1.html" target="_blank">
+				<li class="center-option-mainContent">
+					<img src="<?php echo NIANHUI_STATIC?>img/hz.png" class="center-option-mainContent-img"/>
+					<div class="center-option-bg-three"></div>
+					<div class="center-option-mainContent-font">
+						粤来越爱，慧趣惠州
+					</div>
+				</li>
+			</a>
+			<a href="http://bbs.xizi.com/thread-4379191-1-1.html" target="_blank">
+				<li class="center-option-mainContent middle">
+					<img src="<?php echo NIANHUI_STATIC?>img/syw.png" class="center-option-mainContent-img"/>
+					<div class="center-option-bg-four"></div>
+					<div class="center-option-mainContent-font">
+						你要的诗未必在远方，来惠州的海边从日出浪到日落吧
+					</div>
+				</li>
+			</a>
+			<a href="http://bbs.xizi.com/thread-4378716-1-1.html" target="_blank">
+				<li class="center-option-mainContent">
+					<img src="<?php echo NIANHUI_STATIC?>img/hz.png" class="center-option-mainContent-img"/>
+					<div class="center-option-bg-five"></div>
+					<div class="center-option-mainContent-font">
+						半城山色半城湖，我爱惠州美景
+					</div>
+				</li>
+			</a>
+		</ul>
+	</div>
+	<div class="center-join-title"><font style="background-color: #ffc640;border-radius: 10px;font-size: 30px;">&nbsp;建言献策区&nbsp;</font></div>
+	<div class="center-join">
+		<ul class="ul">
+			<li class="center-join-content-main"><img src="<?php echo NIANHUI_STATIC?>img/female.png" style="width:29px;margin-right: 10px;position: relative;top: 4px;left: 0px;" />珏对萝卜：</li>
+			<li class="center-join-content-main">
+				<font style="font-size: 40px;color: #fff;">“</font>
+				拿近的来说，近年来慢慢起来的“祝屋巷”我觉得是一个很好的地点，逛完西湖，或者逛西湖前都可以去到这个地方，买买吃的，这条街慢慢有很多小清新的店铺进驻，然而还没有形成规模。
+				<font style="display: inline-block;font-size: 40px;color: #fff;float: right;">”</font>
+			</li>
+			<li class="center-join-content-main"><img src="<?php echo NIANHUI_STATIC?>img/male.png" style="width:29px;margin-right: 10px;position: relative;top: 3px;left: 0px;"/>惠ZY洲：</li>
+			<li class="center-join-content-main">
+				<font style="font-size: 40px;color: #fff;">“</font>
+				在惠州部分线路的公交车上让人惊喜地有英文报站，如果要想吸引更多游客，不妨尝试更多语言甚至方言进行报站，这样会让旅游感到更多的归属感。是不是可以考虑下增加下客家话和英文的报站？
+				<font style="display: inline-block;font-size: 40px;color: #fff;float: right;">”</font>
+			</li>
+		</ul>
+		<div class="center-join-button"><a href="http://bbs.xizi.com/forum-168-1.html" target="_blank">我有话说</a></div>
+	</div>
+	
+</div>
+<div class="bottom">
+	<div class="bottom-title">
+		<font class="bottom-title-firstFont">如何成为</font><font style="font-size: 30px;">惠州全域旅游体验达人</font>
+	</div>
+	<div class="bottom-content">
+		<div class="bottom-content-topTitle">旅游达人</div>
+		<ul class="bottom-content-main">
+			<li class="bottom-content-main-li">
+				<div style="color: #474747; font-size: 28px;">建议狂<font style="width: 0.5em; display: inline-block;"></font><img src="<?php echo NIANHUI_STATIC?>img/pen.png" /></div>
+				<div style="color: #838383; font-size: 18px; margin-bottom: 10px;margin-top: 10px;">通过「西子圈」,在“全域旅游”专题下“旅游 Talk”发建议帖,分享你的建言献策。在这里你可以反馈问题,提出建议,定时会收到问题反馈和恢复沟通。</div>
+				<div style="color: #838383; font-size: 18px;margin-top: 10px;"><div class="bottom-content-main-li-comment">评论标准：</div><div>提出惠州旅行问题反馈+可行性建议。</div></div>
+			</li>
+			<li class="bottom-content-main-li">
+				<div style="color: #474747; font-size: 28px;">旅行家<font style="width: 0.5em; display: inline-block;"></font><img src="<?php echo NIANHUI_STATIC?>img/camera.png" /></div>
+				<div style="color: #838383; font-size: 18px;margin-bottom: 10px;margin-top: 10px;">通过「西子圈」,在“全域旅游”专题下“游记 Share”发游记帖,内容：惠州游记（吃住行游购娱）+游玩改善建议。</div>
+				<div style="color: #838383; font-size: 18px;margin-top: 10px;"><div class="bottom-content-main-li-comment">评论标准：</div><div>1.图文并茂，不低于10张照片或者不低于500字；2.两月至少有2篇的游记建议。</div></div>
+			</li>
+		</ul>
+		<div class="bottom-content-bottomTitle">全域旅游体验达人</div>
+	</div>
+	<ul class="bottom-tips">
+		<li class="bottom-tips-first">TIPS:</li>
+		<li class="bottom-tips-content">获得「惠州全域旅游体验达人」认证称号标识，免费体验不同项目的惠州旅游活动，针对体验项目给予可行合理意见，赢取旅游豪华大礼包：证书+奖金+奖金相关奖品。</li>
+	</ul>
+</div>
+<script>
+	var phoneWidth = parseInt(window.screen.width);
+	var phoneHeight = parseInt(window.screen.height);
+	
+	var phoneScale = phoneWidth/750;//除以的值按手机的物理分辨率
+	var ua = navigator.userAgent; 
+	if (/Android (\d+\.\d+)/.test(ua)) { var version = parseFloat(RegExp.$1);
+		if (version > 2.3) {
+			document.write('<meta name="viewport" content="width=device-width,initial-scale='+phoneScale+',minimum-scale='+phoneScale+',maximum-scale='+phoneScale+',user-scalable=no">'); // andriod 2.3以上 
+		} 
+		else {
+			document.write('<meta name="viewport" content="width=device-width,user-scalable=no">'); 
+		} 
+	}else { 
+		document.write('<meta name="viewport" content="width=device-width, initial-scale='+phoneScale+',minimum-scale='+phoneScale+',maximum-scale ='+phoneScale +',user-scalable=no,">'); 
+	}
+	wx.config({
+        debug: false,
+        appId: '<?= $signPackage['appId']?>',
+        timestamp: '<?= $signPackage['timestamp']?>',
+        nonceStr: '<?= $signPackage['nonceStr']?>',
+        signature: '<?= $signPackage['signature']?>',
+        jsApiList: [
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage'
+        ]
+    });
+        wx.ready(function () {
+        wx.onMenuShareAppMessage({
+            title: '全域旅游体验达人',
+            desc: '通过「西子圈」,在“全域旅游”专题下“旅游 Talk”发建议帖,分享你的建言献策。',
+            link:  'http://plus2.hz0752.cn/2018/05/xizitravel',
+            imgUrl: 'http://plus2.hz0752.cn/statics/2018/05/xizitravel/img/share.png',
+            trigger: function (res) {},
+            success: function (res) {},
+            cancel: function (res) {},
+            fail: function (res) {}
+        });
+
+
+        // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
+
+        wx.onMenuShareTimeline({
+            title: '全域旅游体验达人',
+            link: 'http://plus2.hz0752.cn/2018/05/xizitravel',
+            imgUrl: 'http://plus2.hz0752.cn/statics/2018/05/xizitravel/img/share.png',
+            trigger: function (res) {},
+            success: function (res) {},
+            cancel: function (res) {},
+            fail: function (res) {}
+        });
+
+    });
+
+    wx.error(function(res){
+        console.log(res);
+    });
+</script>
